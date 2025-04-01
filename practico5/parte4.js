c6c01739c9a0b0dbd2a181c4c9d9bc51d8123da0
CamiloMoraGDW <cmora@godoworks.com>
2025-04-01 20:17:59 -0300

    ejer4 corregido

--- practico5/parte4.js
+++ practico5/parte4.js
@@ -1,9 +1,11 @@
-console.log("Ingrese un numero");
-let num = parseInt(prompt("Numero: "));
-let suma = 0
-for (i=1; i<num; i+2) {
-    if (i % 3 === 0){
-        suma += i;
-    }
+let num = parseInt(prompt("Ingrese un numero"));
+let suma = 0;
+for (i = 3; i <= num; i + 2) {
+  if (i % 3 === 0) {
+    suma += i;
+  }
 }
-console.log(`La suma de los impares multiplos de 3 es: ${suma}`)
+
+console.log(
+  `La suma de los impares multiplos de 3 entre 1 y ${num} es: ${suma}`
+);
